bb40473f553418210840beeb2b442fb40c3d2258
Satoshi Watanabe <sassy.watanabe@gmail.com>
2013-07-02 19:09:35 +0900

    first commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MomocloService::Application.config.secret_token = '5c36f3dd634f2da8e3645e26c4dcf824a131628300218c0165bf93473be7efb4a513a75bcb2ccacae084899f327803a2bb0ff0b79c01d39ed2590defa80b7c00'
